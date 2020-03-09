ee9ef6eeb33bb911667e64138dc4016f951e1c55
Seung Beom Baek <bitrate256@users.noreply.github.com>
2020-03-09 17:20:16 +0900

    핸드폰 커밋 테스트
    회사에서 작성한 예제 스크립트 옮김.

--- example/01.echo.sh
+++ example/01.echo.sh
@@ -3,3 +3,8 @@ echo "echo Test" # 자동 개행
 printf "printf Test" # 자동 개행 x
 printf "%s %s" printf test # 뒤에 오는 문자열들이 전달되는 인자임
 
+echo "테스트 메세지"
+echo "테스트 ''메세지''"
+echo "테스트 '메세지'"
+
+# end of script
---
+++ example/02.echo(메세지,데이터 함께 출력).sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# This script displays the date and logged in log
+echo "이 다음 메세지는 데이터와 함께 출력됨."
+echo -n "The time and date are : "
+date
+echo "logged in log : "
+lastlog
