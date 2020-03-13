6774e3d5d24f13f9684b1c34e10bb4e697d24a87
Seung Beom Baek <bitrate256@users.noreply.github.com>
2020-03-13 10:51:33 +0900

    03.variable
    내용 추가

--- reference_book_example/04.command.sh
+++ reference_book_example/04.command.sh
@@ -4,5 +4,13 @@
 testing='date'
 echo "The date are : " testing
 #$() 형식
+#다음은 쉘 명령으로부터 출력을 받아 변수를 만든다.
 testing=$(date)
 echo "The date are : "$testing"
+
+#날짜를 저장하고 스크립트에 고유한 파일 이름을 만든다.
+copy the /usr/bin directory listing to a log file
+today=$(date +%y%m%d)
+ls /usr/bin -al > log.$today
+#today 변수가 date 명령의 형식화된 출략을 보관함. 로그 파일 이름으로부터 날짜 정보를 추출할 때 널리 사용되는 기법임.
+# +%y%m%d 형식은 두 자라 수의 년,월,일 로 날짜 형식을 지시함.
